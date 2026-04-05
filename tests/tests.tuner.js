c792efd602de022b78125d4de2cf82ca9ce6ae58
Mathieu Ducharme <mat@locomotive.ca>
2026-04-05 07:30:05 -0400

    Deleted outdated proof-of-concept

--- tests/tests.tuner.js
+++
@@ -1,9 +0,0 @@
-/**
-* Unit tests for Tuner
-*/
-QUnit.module("Tuner");
-
-// Simple test to ensure everything has been included properly
-QUnit.test("Tuner class", function(assert) {
-	assert.ok(Tuner, "Main namespace exists");
-});
